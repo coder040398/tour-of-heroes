eaba04c538734aef6c219e9c108a21267e4c7395
Rino <rino@Rinos-MacBook-Air.local>
2023-04-04 16:17:07 +0700

    create message component and service

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { Hero } from './hero';
-import { HEROES } from '../mock-heroes';
 import { HeroService } from './hero.service';
+import { MessageService } from '../messages/message.service';
 
 @Component({
   selector: 'app-heroes',
@@ -9,7 +9,7 @@ import { HeroService } from './hero.service';
   styleUrls: ['./heroes.component.css'],
 })
 export class HeroesComponent {
-  constructor(private heroService: HeroService) {}
+  constructor(private heroService: HeroService, private messageService: MessageService) {}
 
   heroes: Hero[] = [];
   selectedHero?: Hero;
@@ -24,5 +24,6 @@ export class HeroesComponent {
 
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
+    this.messageService.add(`HeroesComponent: Selected hero id=${hero.id}`);
   }
 }
